bd79eaebb1a38cfde6a9e6d53d163cef150e47ab
takatoh <takatoh@mbg.nifty.com>
2010-05-15 22:12:28 +0900

    lib/photo_storage.rb: Change path to photo, sample, thumbnail.

--- lib/photo_storage.rb
+++ lib/photo_storage.rb
@@ -31,8 +31,8 @@ class PhotoStorage
   end
 
 
-  def store(content, path)
-    fullpath = photo_fullpath(path)
+  def store(content, filename)
+    fullpath = photo_fullpath(filename)
     dir = File.dirname(fullpath)
     unless File.exist?(dir)
       FileUtils.mkdir_p(dir)
@@ -40,10 +40,10 @@ class PhotoStorage
     File.open(fullpath, "wb") do |f|
       f.write(content)
     end
-    md5 = Digest::MD5.hexdigest(content) + ".#{@format}"
-    thumb_path = make_thumbnail(fullpath, {:name => md5})
-    sample_path = make_sample(fullpath, {:name => md5})
-    [ photo_path(path), thumb_path, sample_path ]
+    name = filename.sub(File.extname(filename), ".#{@format}")
+    thumb_path = make_thumbnail(fullpath, {:name => name})
+    sample_path = make_sample(fullpath, {:name => name})
+    [ photo_path(filename), thumb_path, sample_path ]
   end
 
 
@@ -65,6 +65,9 @@ class PhotoStorage
   def make_sample(photopath, opts = {})
     samplepath = sample_fullpath(opts[:name])
     return sample_path(opts[:name]) if File.exist?(samplepath) and !opts[:force]
+    unless File.exist?(File.dirname(samplepath))
+      FileUtils.mkdir_p(File.dirname(samplepath))
+    end
     img = Magick::Image.read(photopath).first
     if img.columns > 600 or img.rows > 800
       geometry = Magick::Geometry.from_s("600x800")
@@ -82,28 +85,28 @@ class PhotoStorage
 
   private
 
-  def photo_path(path)
-    File.join(@photo_dir, path)
+  def photo_path(filename)
+    File.join(@photo_dir, filename.slice(0,2), filename.slice(2,2), filename)
   end
 
-  def photo_fullpath(path)
-    File.join(@storage_dir, @photo_dir, path)
+  def photo_fullpath(filename)
+    File.join(@storage_dir, photo_path(filename))
   end
 
-  def thumb_path(path)
-    File.join(@thumbnail_dir, path.sub(File.extname(path), ".#{@format}"))
+  def thumb_path(filename)
+    File.join(@thumbnail_dir, filename.slice(0,2), filename.slice(2,2), filename)
   end
 
-  def thumb_fullpath(path)
-    File.join(@storage_dir, thumb_path(path))
+  def thumb_fullpath(filename)
+    File.join(@storage_dir, thumb_path(filename))
   end
 
-  def sample_path(path)
-    File.join(@sample_dir, path.sub(File.extname(path), ".#{@format}"))
+  def sample_path(filename)
+    File.join(@sample_dir, filename.slice(0,2), filename.slice(2,2), filename)
   end
 
-  def sample_fullpath(path)
-    File.join(@storage_dir, sample_path(path))
+  def sample_fullpath(filename)
+    File.join(@storage_dir, sample_path(filename))
   end
 
   def local_encoding(path)
