861dc3ec167658402701b16b614c67bf3ec614ac
takatoh <takatoh.m@gmail.com>
2010-08-21 20:49:16 +0900

    lib/file_fetcher.rb: retry if status is 302.

--- lib/file_fetcher.rb
+++ lib/file_fetcher.rb
@@ -14,16 +14,25 @@ class FileFetcher
   include ImageTypes
 
   class NotImage < StandardError; end
+  class FetchError < StandardError; end
 
   def self.fetch(url, opts = {})
     client = HTTPClient.new
     h = client.head(url)
-    if opts[:ignore_media_type] || IMAGE_CONTENT_TYPES.member?(h.contenttype)
-      c = client.get(url)
-      filename = File.basename(url)
-      { :filename => filename, :body => c.content }
+    case h.status
+    when 200     # OK
+      if opts[:ignore_media_type] || IMAGE_CONTENT_TYPES.member?(h.contenttype)
+        c = client.get(url)
+        filename = File.basename(url)
+        { :filename => filename, :body => c.content }
+      else
+        raise NotImage.new("Content-Type: #{h.contenttype}")
+      end
+    when 302     # Moved Temporarily
+      location = h.header["Location"].first
+      fetch(location, opts)
     else
-      raise NotImage.new("Content-Type: #{h.contenttype}")
+      raise FetchError.new("Status: #{h.status}: #{url}")
     end
   end
 
