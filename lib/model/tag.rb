83dc25b13956314cc8f31cedc04bdf06cd17f4a2
takatoh <takatoh.m@gmail.com>
2010-09-12 18:56:36 +0900

    Add alias to tag (db schema and model class).

--- lib/model/tag.rb
+++ lib/model/tag.rb
@@ -3,5 +3,34 @@ require 'boot'
 class Tag < Sequel::Model
   one_to_many :taggings
 
+
+  def add_alias(tag)
+    if tag.class == String
+      tag = Tag.find(:name => tag) || Tag.new(:name => tag).save
+    end
+    tag.alias_to = self.id
+    self.has_alias = true
+    tag.save
+    self.save
+  end
+
+  def remove_alias(alias_tag)
+    if alias_tag.class == String
+      alias_tag = Tag.find(:name => alias_tag)
+    end
+    alias_tag.alias_to = nil
+    alias_tag.save
+    if Tag.find(:alias_to => self.id).nil?
+      self.has_alias = false
+      self.save
+    else
+      self
+    end
+  end
+
+  def aliases
+    Tag.filter(:alias_to => self.id).all || []
+  end
+
 end
 
