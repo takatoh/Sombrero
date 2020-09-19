8c8e54ce129526626b8b11ed2394e439904fb2da
takatoh <takatoh.m@gmail.com>
2020-09-19 17:17:48 +0900

    boot.rb: Simplify.

--- boot.rb
+++ boot.rb
@@ -1,14 +1,9 @@
-require 'rubygems'
 require 'sequel'
 require 'yaml'
 
-unless defined?(SOMBRERO_ROOT)
-  SOMBRERO_ROOT = File.dirname(__FILE__)
-  $LOAD_PATH.unshift(SOMBRERO_ROOT + "/lib")
-end
+SOMBRERO_ROOT = File.dirname(__FILE__)
+$LOAD_PATH.unshift(SOMBRERO_ROOT + "/lib")
 
-SOMBRERO_CONFIG = YAML.load_file("config.yaml") unless defined?(SOMBRERO_CONFIG)
+SOMBRERO_CONFIG = YAML.load_file("config.yaml")
 
-DB = Sequel.connect(SOMBRERO_CONFIG["database"]) unless defined?(DB)
-
-
+DB = Sequel.connect(SOMBRERO_CONFIG["database"])
