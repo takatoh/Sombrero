475a6ac1ec63ed8365c1e46eb1d4335556049949
takatoh <takatoh.m@gmail.com>
2024-03-03 15:19:41 +0900

    bin/dump_posts.rb: Define main method.

--- bin/dump_posts.rb
+++ bin/dump_posts.rb
@@ -1,13 +1,18 @@
-#! ruby -Ku
+require "./boot"
+require "model"
 
-require 'boot'
-require 'model'
 
-posts = Post.order_by("id").all
+def main
+  posts = Post.order_by("id").all
 
-posts.each do |p|
-  puts "- url: #{p.url}"
-  puts "  page_url: #{p.page_url}"
-  puts "  file: #{p.photo.path}"
-  puts "  tags: #{p.photo.taggings.map{|t| t.tag.name}.join(' ').inspect}"
+  posts.each do |p|
+    puts "- url: #{p.url}"
+    puts "  page_url: #{p.page_url}"
+    puts "  file: #{p.photo.path}"
+    puts "  tags: #{p.photo.taggings.map{|t| t.tag.name}.join(' ').inspect}"
+  end
 end
+
+
+
+main
