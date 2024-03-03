1f3f7e6c06086521b45dc0e0d0ac649d45916840
takatoh <takatoh.m@gmail.com>
2024-03-03 14:49:41 +0900

    bin/list_photos.rb: Define main method.

--- bin/list_photos.rb
+++ bin/list_photos.rb
@@ -2,21 +2,27 @@ require "./boot"
 require "model/photo"
 require "model/post"
 
-ds = Photo.order_by("id")
-photos = ds.all
 
-photos.each do |photo|
-  puts "#{photo[:id]}:"
-  puts "  size:        #{photo[:width]}x#{photo[:height]}"
-  puts "  file size:   #{photo[:filesize]}"
-  puts "  md5:         #{photo[:md5]}"
-  puts "  sha256:      #{photo[:sha256]}"
-  puts "  path:        #{photo[:path]}"
-  puts "  sample path: #{photo[:sample_path]}"
-  puts "  thumb path:  #{photo[:thumbnail_path]}"
-  puts "  posts:"
-  photo.posts.each do |post|
-    puts "    [#{post.id}]  #{post.url}"
+def main
+  ds = Photo.order_by("id")
+  photos = ds.all
+
+  photos.each do |photo|
+    puts "#{photo[:id]}:"
+    puts "  size:        #{photo[:width]}x#{photo[:height]}"
+    puts "  file size:   #{photo[:filesize]}"
+    puts "  md5:         #{photo[:md5]}"
+    puts "  sha256:      #{photo[:sha256]}"
+    puts "  path:        #{photo[:path]}"
+    puts "  sample path: #{photo[:sample_path]}"
+    puts "  thumb path:  #{photo[:thumbnail_path]}"
+    puts "  posts:"
+    photo.posts.each do |post|
+      puts "    [#{post.id}]  #{post.url}"
+    end
   end
 end
 
+
+
+main
