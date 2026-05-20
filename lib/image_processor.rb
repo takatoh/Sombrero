17f0b9fd33b5baebe0b090752427b58aea2cf1c1
takatoh <takatoh.m@gmail.com>
2026-05-20 23:08:34 +0900

    lib/image_processor.rb: Pass String for filename.

--- lib/image_processor.rb
+++ lib/image_processor.rb
@@ -8,19 +8,19 @@ class ImageProcessor
 
   # Get the dimensions of an image
   def self.get_image_size(filename)
-    image = Vips::Image.new_from_file(filename)
+    image = Vips::Image.new_from_file(filename.to_s)
     [image.width, image.height]
   end
   
   # Resize an image (downsize only)
   def self.downsize_to_limit(filename, thumb_width, thumb_height, thumb_filename)
     thumb = Vips::Image.thumbnail(
-        filename,
+        filename.to_s,
         thumb_width,
         height: thumb_height,
         size: :down
     )
-    thumb.write_to_file(thumb_filename)
+    thumb.write_to_file(thumb_filename.to_s)
     [thumb.width, thumb.height]
   end
 
